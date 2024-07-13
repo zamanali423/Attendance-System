6b074f4377c40ea2d51aafb697ad0991d51d485a
Zaman Ali <138426327+zamanali423@users.noreply.github.com>
2024-07-12 22:43:45 -0700

    Update App.js

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,24 +1,25 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './pages/Home';
-import Navbar from './components/Navbar';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import Provider from './context/userContext.js/Provider';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import SystemReport from "./pages/SystemReport";
 
 function App() {
   return (
     <>
-      <Provider>
       <BrowserRouter>
-      <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/signup' element={<Signup/>}/>
-          <Route path='/login' element={<Login/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/generatereport" element={<SystemReport />} />
         </Routes>
       </BrowserRouter>
-      </Provider>
     </>
   );
 }
