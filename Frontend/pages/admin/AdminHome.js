333ef18dceb687fcd1e79f4c6aff14bfac039b40
Zaman Ali <138426327+zamanali423@users.noreply.github.com>
2024-07-12 22:47:52 -0700

    Create AdminHome.js

---
+++ Frontend/pages/admin/AdminHome.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import GetStudents from './components/GetStudents'
+
+const AdminHome = () => {
+  return (
+    <div>
+      <GetStudents/>
+    </div>
+  )
+}
+
+export default AdminHome
